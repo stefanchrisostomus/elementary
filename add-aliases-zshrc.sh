ba1422b7e4dda1ff94a9a21cd4f37ea7b432892c
Stefan Chrisostomus <stefanchrisostomus13@gmail.com>
2020-03-10 13:49:49 +0100

    Comment : aded aliases script on Tue Mar 10 13:49:49 CET 2020

---
+++ add-aliases-zshrc.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+###############################################################################
+# This script is to be used on 64 bit computers                               #
+# Author: Stefanski1977                                                       #
+# Date: 10-03-2020                                                            #
+# This script wil: Add my aliases to .zshrc                                   #
+###############################################################################
+#                                                                             #
+# Examine before running this script. Run at your own risk.                   #
+#                                                                             #
+###############################################################################
+
+echo "# My Aliases    
+alias c='clear'
+alias speed='speedtest-cli'
+alias lsa='ls -a'
+alias home='cd /home/stefan/'
+alias desk='cd /home/stefan/Desktop/'
+alias doc='cd /home/stefan/Documents/'
+alias gith='cd /home/stefan/Documents/github/'
+alias down='cd /home/stefan/Downloads/'
+alias update='sudo apt update && sudo apt upgrade -y'" >> .zshrc 
