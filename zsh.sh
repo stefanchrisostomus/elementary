6e5981e291809425ae4d3f5e26388a5ef30fa8d7
Stefan Chrisostomus <stefanchrisostomus13@gmail.com>
2020-03-24 11:03:27 +0100

    Updated zsh script

--- zsh.sh
+++ zsh.sh
@@ -10,39 +10,25 @@ set -e
 # Examine before running this script. Run at your own risk.                   #
 #                                                                             #
 ###############################################################################
-#                                                                             #  
-#                                                                             #
-#                                                                             #
-#  Copyright (C) 2020 Stefanski1977                                           #
-#                                                                             #
-#  This program is free software: you can redistribute it and/or modify       #
-#  it under the terms of the GNU General Public License as published by       #
-#  the Free Software Foundation, either version 3 of the License, or          #
-#  (at your option) any later version.                                        #
-#                                                                             #
-#  This program is distributed in the hope that it will be useful,            #
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of             #
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              #
-#  GNU General Public License for more details.                               #
-#                                                                             #
-#  You should have received a copy of the GNU General Public License          #
-#  along with this program.  If not, see <https://www.gnu.org/licenses/>.     #
-#                                                                             #
-#                                                                             #
-###############################################################################   
 
 # Install zsh
 sudo apt-get install zsh git wget -y
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 
 # Install classyTouch theme
-git clone https://github.com/yarisgutierrez/classyTouch_oh-my-zsh.git ~/.oh-my-zsh/themes
+git clone https://github.com/yarisgutierrez/classyTouch_oh-my-zsh.git 
+
+# Changing directory
+cd classyTouch_oh-my-zsh
+
+# Copy theme to theme folder
+cp classyTouch.zsh-theme ~/.oh-my-zsh/themes
 
 # Changing the theme to classyTouch
 sudo sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"classyTouch\"/g' ~/.zshrc
 
 # Changing shell to zshrc
-chsh -s $(which zsh)
+chsh -s /bin/zsh stefan
 
 
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
